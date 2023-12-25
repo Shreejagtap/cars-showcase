40fc1921662bc303b6d9afb318a3941f957ecf93
Shreejagtap <94234886+Shreejagtap@users.noreply.github.com>
2023-12-26 00:28:40 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 // Dark Mode Toggle Function
 const darkMode = () => {
-    const mode = document.getElementById("mainHTML")
-    if (mode.classList.contains("dark")) {
-        mode.classList.remove("dark")
-        document.getElementById("mainhead").classList.remove("white-shadow")
-        document.getElementById("dark-button").innerHTML = `<i class="fa-solid fa-moon"></i>`
-        document.getElementById("hero-container").innerHTML = `<img
+  const mode = document.getElementById('mainHTML');
+  if (mode.classList.contains('dark')) {
+    mode.classList.remove('dark');
+    document.getElementById('mainhead').classList.remove('white-shadow');
+    document.getElementById(
+      'dark-button'
+    ).innerHTML = `<i class="fa-solid fa-moon moon-animation"></i>`;
+    document.getElementById('hero-container').innerHTML = `<img
         src="/images/QUANTUMLIGHT.png"
         alt=""
         id="heroimg"
-        />`
-    }
-    else {
-        mode.classList.add("dark")
-        document.getElementById("mainhead").classList.add("white-shadow")
-        document.getElementById("dark-button").innerHTML = `<i class="fa-solid fa-sun"></i>`
-        document.getElementById("hero-container").innerHTML = `<img
+        />`;
+  } else {
+    mode.classList.add('dark');
+    document.getElementById('mainhead').classList.add('white-shadow');
+    document.getElementById(
+      'dark-button'
+    ).innerHTML = `<i class="fa-solid fa-sun sun-animation"></i>`;
+    document.getElementById('hero-container').innerHTML = `<img
           src="/images/QUANTUMDARK.png"
           alt=""
           id="heroimg"
-        />`
-    }
-}
+        />`;
+  }
+};
 
 // Mobile Nav On Off Function
 const mobNav = () => {
-    const nav = document.getElementById("mobile")
-    if (nav.classList.contains("open")) {
-        nav.classList.remove("open")
-        document.getElementById("hamburgerico").innerHTML = `<i class="fa-solid fa-bars"></i>`
-    }
-    else {
-        nav.classList.add("open")
-        document.getElementById("hamburgerico").innerHTML = `<i class="fa-solid fa-xmark"></i>`
-    }
-
-}
+  const nav = document.getElementById('mobile');
+  if (nav.classList.contains('open')) {
+    nav.classList.remove('open');
+    document.getElementById(
+      'hamburgerico'
+    ).innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    nav.classList.add('open');
+    document.getElementById(
+      'hamburgerico'
+    ).innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+};
